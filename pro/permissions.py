8d9f9ce9ef307de1a0f08427972b9d3c355849b5
iyerikuzwe <iyerikuzweregine19@gmail.com>
2019-04-01 14:36:30 +0200

    add if statement in authentication class in permissions.py

--- pro/permissions.py
+++ pro/permissions.py
@@ -2,7 +2,7 @@ from rest_framework.permissions import SAFE_METHODS, BasePermission
 
 class IsAuthenticatedOrReadOnly(BasePermission):
     def has_permission(self, request, view):
-        if request.method in SAFE_METHODS:.
+        if request.method in SAFE_METHODS:
             return True
         else:
             return request.user.is_staff
