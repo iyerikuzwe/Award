0b9d27a16c4f112f09e722bdf024a68cd58ed77b
iyerikuzwe <iyerikuzweregine19@gmail.com>
2019-04-04 15:24:42 +0200

    add changes

--- rate/settings.py
+++ rate/settings.py
@@ -22,9 +22,9 @@ if config('MODE')=="dev":
    DATABASES = {
        'default': {
            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-           'NAME': config('DB_NAME'),
-           'USER': config('DB_USER'),
-           'PASSWORD': config('DB_PASSWORD'),
+           'NAME':'rate',
+           'USER':'wecode',
+           'PASSWORD':'regine',
         #    'HOST': config('DB_HOST'),
         #    'PORT': '',
        }    
